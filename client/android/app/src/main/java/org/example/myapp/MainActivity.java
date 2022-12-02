76a6aeef13ee8926bfab35434ce901063adcbf1e
vc174887 <vc174887@fh-muenster.de>
2022-12-02 14:48:38 +0100

    added capacitor

---
+++ client/android/app/src/main/java/org/example/myapp/MainActivity.java
@@ -0,0 +1,5 @@
+package org.example.myapp;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ client/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'org.example.myapp',
+  appName: 'MyApp',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
