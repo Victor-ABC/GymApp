6cb47007e4b92deb264743886db68eb49c028a7e
Pascal Thesing <pascal@thesing.me>
2023-01-15 20:52:17 +0100

    Add-vite-pwa-plugin

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,11 +3,34 @@
 import { defineConfig } from 'vite';
 import postcssPresetEnv from 'postcss-preset-env';
 import viteLitCssPlugin from './vite-lit-css-plugin.js';
-import viteHtmlReplacePlugin from './vite-html-replace-plugin.js';
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [
     viteLitCssPlugin(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: [
+        'avatar.png',
+        'favicon.png',
+        'no-image.png',
+        'standardUploadImage.png'
+      ],
+      manifest: {
+        name: 'Gym+',
+        short_name: 'Gym+',
+        description: 'Gym+',
+        theme_color: '#4a968b',
+        background_color: '#728380',
+        icons: [
+          {
+            src: 'favicon.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
   ],
   server: { port: 8080 },
   css: {
